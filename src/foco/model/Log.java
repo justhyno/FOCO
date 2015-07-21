2cbd5b5a3a09a084779ff74c9f773894fa7b7bfb
justhyno <Estudante-9@Estudante-9-PC>
2015-07-21 17:07:22 +0200

    first version Signed-off-by:justhyno <Estudante-9@Estudante-9-PC> Signed-off-by:justhyno <Estudante-9@Estudante-9-PC>

--- src/foco/model/Log.java
+++ src/foco/model/Log.java
@@ -17,5 +17,37 @@ public class Log {
     Date data;
     String descricao;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     
 }
